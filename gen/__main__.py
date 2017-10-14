796fc345e0eb3bdb3f90d7ca8d64a0e6c9bc162d
Adam Svystun <adam.svystun@gmail.com>
2017-10-14 19:46:12 +0800

    Add comments

--- gen/__main__.py
+++ gen/__main__.py
@@ -3,9 +3,10 @@ from datetime import timedelta, datetime
 from matplotlib import pyplot as mp
 import numpy as np
 
-from .math import (
+from gen.math import (
     random_parameters, number_of_demands_per_day, gaussian_d, normalize_day
 )
+from gen.file import write
 
 # Number of authors 5
 # author #1 20      80 10       Fiction
@@ -38,37 +39,50 @@ types = [
     [50, 10000]
 ]
 def gen():
+    data = []
     for i in range(100):
+        # Get a random author
         author = np.random.randint(0, 5, 1)[0]
+        # Get list of types of books this author writes
         author_type = author_types[author]
+        # Get type of the book from the list randomly
+        # If the length of the list is 1 -> no need to randomize
         if len(author_type) == 1:
             book_type = author_type[0]
         else:
             random_type_i = np.random.randint(0, 1, 1)[0]
             book_type = author_type[random_type_i]
+        # Get this authors gaussian params
         g_author = authors[author]
+        # Get this book type gaussian params
         g_type = types[book_type]
+        # Randomize gaussian params
         type_mu, type_sig = random_parameters(g_type[0], g_type[1])
         author_mu, author_sig = random_parameters(g_author[0], g_author[1])
+        # Randomize the demand
         demands = np.random.randint(2000, 3000, 1)[0]
+        # Generate the demand hours through the year
         hours = generate_hours(
             demands, type_mu, type_sig, author_mu, author_sig
         )
-        for i in range(len(hours)-1):
-            hour = hours[i]
-            solution = hours[i+1] - hour
+        for j in range(len(hours)-1):
+            hour = hours[j]
+            solution = hours[j+1] - hour
             current_hour = hour % 24
             day = ((hour - current_hour) / 24)
             current_day_of_week += day % 7
             current_week += (day - current_day_of_week) / 7
-            print([
+            line = [
                 author,
                 i,
                 current_week,
                 current_day_of_week,
                 current_hour,
-                book_type
-            ])
+                book_type,
+                solution
+            ]
+            data.append(line)
+    write("data/input.data", data)
 
 
 
