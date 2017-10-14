7a5d4a8cc6c1f5f74e0d522ac04145ebd47ae9e2
Adam Svystun <adam.svystun@gmail.com>
2017-10-14 16:42:16 +0800

    Fix bugs

--- gen/__main__.py
+++ gen/__main__.py
@@ -1,18 +1,13 @@
 from matplotlib import pyplot as mp
 import numpy as np
 
-from .math import gaussian, gaussian_f
+from .math import gaussian_d
 
 def main():
-    number_of_points = 70
+    number_of_points = 8736
     # 8736
-    g = gaussian_f(np.linspace(0, 8736, number_of_points), 2500, 100)
-    # mp.plot()
-    d = []
-    d.append(0)
-    for i, point in enumerate(g):
-        d.append(d[i] + point)
-    d = d[:number_of_points]
+    d = gaussian_d(11, 5, 5, 24)
+    print(d)
     mp.plot(d, np.zeros_like(d), "x")
     mp.show()
 
--- gen/math.py
+++ gen/math.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as mp
 
 def gaussian_f(x, mu, sig):
     return 1 - np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
@@ -7,3 +8,17 @@ def gaussian(mu, sigma, number_of_points):
     g = np.random.normal(mu, sigma, number_of_points)
     g = [ int(x) for x in g ]
     return g
+
+def gaussian_d(mu, sig, n, scale):
+    g = gaussian_f(np.linspace(0, scale, n), mu, sig)
+    mp.plot(g)
+    mp.show()
+    d = []
+    d.append(0)
+    for i, point in enumerate(g):
+        d.append(d[i] + point)
+    d = d[1:]
+    rel_scale = scale/d[n-1]
+    for point in d:
+        point = point * rel_scale
+    return d
