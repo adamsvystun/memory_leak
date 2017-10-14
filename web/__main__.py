d39e9c292dc721203d0423c1a177f369bc459759
Adam Svystun <adam.svystun@gmail.com>
2017-10-14 22:22:34 +0800

    Update data

--- web/__main__.py
+++ web/__main__.py
@@ -10,10 +10,28 @@ def hello_world(request):
         content_type='text/html'
     )
 
+def index_css(request):
+    return FileResponse(
+        "web/static/index.css",
+        request=request,
+        content_type='text/css'
+    )
+
+def index_js(request):
+    return FileResponse(
+        "web/static/index.js",
+        request=request,
+        content_type='text/javascript'
+    )
+
 if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('hello', '/')
+        config.add_route('css', '/index.css')
+        config.add_route('js', '/index.js')
         config.add_view(hello_world, route_name='hello')
+        config.add_view(index_css, route_name='css')
+        config.add_view(index_js, route_name='js')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
