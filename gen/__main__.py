539591e5f120a99a35fa090eb910d57c01d97e69
Adam Svystun <adam.svystun@gmail.com>
2017-10-14 16:22:12 +0800

    Add gaussian transformation

--- gen/__main__.py
+++ gen/__main__.py
@@ -1,11 +1,19 @@
 from matplotlib import pyplot as mp
 import numpy as np
 
-from .math import gaussian
+from .math import gaussian, gaussian_f
 
 def main():
-    g = [1, 2, 3, 7, 9, 14, 24, 37]
-    mp.plot(g, np.zeros_like(g), "x")
+    number_of_points = 70
+    # 8736
+    g = gaussian_f(np.linspace(0, 100, number_of_points), 50, 10)
+    # mp.plot()
+    d = []
+    d.append(0)
+    for i, point in enumerate(g):
+        d.append(d[i] + point)
+    d = d[:number_of_points]
+    mp.plot(d, np.zeros_like(d), "x")
     mp.show()
 
 if __name__ == "__main__":
--- gen/math.py
+++ gen/math.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def gaussian_f(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+    return 1 - np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 def gaussian(mu, sigma, number_of_points):
     g = np.random.normal(mu, sigma, number_of_points)
